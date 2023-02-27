9482fa0d0ebbeb87cff070a68d64bea03c6c0018
m-aks <m-aks@i.ua>
2023-02-28 01:39:41 +0200

    tours search memory

--- console/controllers/CronController.php
+++ console/controllers/CronController.php
@@ -68,7 +68,7 @@ class CronController extends \yii\console\Controller
 
     public function init()
     {
-        ini_set('memory_limit', '2048');
+        ini_set('memory_limit', '3072');
     }
 
     private static function getCountryId($name)
